15f96daf03dc1d376f427dd3ce49f9e1ce368800
Mario Eguiluz <mario@myplacesandme.com>
2020-03-18 18:01:58 -0400

    remove anti-redirect measures add quotaguard proxy bump version 1.0.17

--- src/scrapedin.js
+++ src/scrapedin.js
@@ -3,13 +3,15 @@ const login = require('./login')
 const profile = require('./profile/profile')
 const logger = require('./logger')
 
+const QUOTA_GUARD_URL = `http://7mn60dah69jyx2:dx7e285lx4aqaobuaan3b1chsgxy0g@proxy.quotaguard.com:9292`
+
 module.exports = async({ cookies, email, password, isHeadless, hasToLog, hasToGetContactInfo, puppeteerArgs } = { isHeadless: true, hasToLog: false }) => {
   if (!hasToLog) {
     logger.stopLogging()
   }
   logger.info('scrapedin', 'initializing')
 
-  const args = Object.assign({ headless: isHeadless, args: ['--no-sandbox'] }, puppeteerArgs)
+  const args = Object.assign({ headless: isHeadless, args: ['--no-sandbox', `--proxy-server=${QUOTA_GUARD_URL}`] }, puppeteerArgs)
   const browser = await puppeteer.launch(args)
 
   if(cookies) {
